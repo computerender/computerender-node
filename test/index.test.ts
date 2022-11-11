5853a5ae3ddaec477c16e9e486157c609f7e1ac8
PWhiddy <all.cows.like.to.moo@gmail.com>
2022-11-11 02:20:44 -0500

    test image gen

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,9 @@
 import { describe, expect, it } from "vitest";
+import fs from "fs";
 
 import { Computerender } from "../dist/index";
+
+const apiKey = process.env.CR_KEY || "invalid";
 
 describe("should", () => {
   it("Instance", () => {
@@ -8,3 +11,18 @@ describe("should", () => {
     expect(instance.apiKey).toEqual("test_key");
   })
 })
+
+describe("should", () => {
+  it("image gen", async () => {
+    const cr = new Computerender(apiKey);
+    const prompt = "cat with sunglasses";
+    const imageResult = await cr.generateImage({prompt});
+    // Optionally write to file 
+    const ws = imageResult.pipe(fs.createWriteStream(prompt + ".jpg"));
+    await new Promise(fulfill => ws.on("finish", fulfill));
+    expect(imageResult).toBeDefined();
+    //expect(instance.apiKey).toEqual("test_key");
+  })
+})
+
+
